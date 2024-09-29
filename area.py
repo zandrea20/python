1798332f019086de80e7b2ae0a602b029c0aa56d
zandrea20 <zandrea@students.mchenry.edu>
2024-09-29 16:31:44 -0500

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+#This program is designed to display the length width and area of a rectangle
+length = 5
+width = 3
+# Area
+area = length * width 
+print(f"The area of the rectangle with a length of {length} and a width of {width} is {area}.")
