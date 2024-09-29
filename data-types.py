11df9ecb20bed3a18730187dda14cecc4e18afcc
zandrea20 <zandrea@students.mchenry.edu>
2024-09-29 15:41:36 -0500

    Add files via upload

---
+++ data-types.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 20
+print("Literal:", x)
+
+# Integer
+y = -40
+print("Integer:", y)
+
+# Float
+z = 18.7
+print("Float:", z)
+
+# String
+name = "Steve"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
