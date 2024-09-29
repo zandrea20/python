e00c440774e39ef4ee6a4e09cebdd792e6e02872
zandrea20 <zandrea@students.mchenry.edu>
2024-09-29 14:43:29 -0500

    Add files via upload

---
+++ knock knock.py
@@ -0,0 +1,2 @@
+#This Program is designed to write a Knock-Knock joke in one line of code using then print function and escape characters.
+print("Knock Knock.\nWho's there?\nNunya.\nNunya who?\nNunya Business!")
