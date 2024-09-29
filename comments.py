bfa4aaf2a7e60010d4a49f6ce6a991857bd1d698
zandrea20 <zandrea@students.mchenry.edu>
2024-09-29 17:10:34 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,5 @@
+#This program can find your miles per gallon via tank size an distance
+tank_size = 15  # Adjust for your vehicle 
+distance = 600 # Adjust for your distance
+mpg = distance / tank_size # This equation gets mpg by taking distance and dividing it by your tank size
+print("Miles per gallon:", mpg)
