ff2b495bab7edac957fa36d78a2957c250c35f42
zandrea20 <zandrea@students.mchenry.edu>
2024-10-25 03:27:42 -0500

    Update PE1 4.3.py

--- PE1 4.3.py
+++ PE1 4.3.py
@@ -1,9 +1,13 @@
-def calculate_interest(principal_amount, interest_rate, investment_time):
-    calculated_interest = principal_amount * (1 + (interest_rate / 100) * investment_time)
-    print(f"The total amount after {investment_time} years for a principal amount of ${principal_amount:,.2f} "
-          f"at an interest rate of {interest_rate}% is: ${calculated_interest:,.2f}")
+def calculate_interest(interest_rate, principal_amount, investment_time):
+    calculated_interest = (principal_amount*investment_time*interest_rate) / 100
+    return calculated_interest
 
-input_principal_amount = float(input("Enter Principal Amount: "))
-input_interest_rate = float(input("Please enter the interest rate as a whole number: "))
-input_investment_time = float(input("Please enter the investment time in whole years: "))
-calculate_interest(input_principal_amount, input_interest_rate, input_investment_time)
+principal_amount = float(input("Please input your principal amount:"))
+interest_rate = float(input("Please input your intrest rate as a whole number (5% would be 5):"))
+investment_time = float(input("Please input your time invested in whole years:"))
+
+calculated_interest = calculate_interest(principal_amount, interest_rate, investment_time)
+
+print(f"The simple interest for a principal amount of ${principal_amount:,.2f} "
+      f"at an interest rate of {interest_rate}% over a period of "
+      f"{investment_time} years is: ${calculated_interest:,.2f}")
