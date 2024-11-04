f57987e9fffd1edd16b634ed44a30c535f31bb29
zandrea20 <zandrea@students.mchenry.edu>
2024-11-03 23:46:18 -0600

    Add files via upload

---
+++ math_opperations/geometry.py
@@ -0,0 +1,10 @@
+import math
+
+def rectangle_area(length, width):
+    return length * width
+
+def triangle_area(base, height):
+    return (base*height)/2
+
+def cirlce_area(radius):
+    return math.py * (radius ** 2)
